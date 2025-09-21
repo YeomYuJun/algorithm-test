1525d53360de62604f4789ecb767030bc4d721b8
YeomYuJun <yyj2046@gmail.com>
2025-09-21 23:07:33 +0900

    [level 2] Title: H-Index, Time: 0.91 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        java.util.Arrays.sort(citations);
+        int n = citations.length;
+        for(int i = 0; i < n; i++){
+            if(citations[i] >= n - i){
+                answer = n - i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
